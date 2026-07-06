8c72c608ed22f637e273ee4e1f92bb30d9397954
Prakhya <gotnochill815@gmail.com>
2026-07-06 20:25:36 +0530

    Update dense.py

--- retrieval/dense.py
+++ retrieval/dense.py
@@ -1,3 +1,4 @@
+import os
 import faiss
 import numpy as np
 from sentence_transformers import SentenceTransformer
@@ -5,13 +6,28 @@ from sentence_transformers import SentenceTransformer
 
 class DenseRetriever:
 
-    def __init__(self, model_name="BAAI/bge-base-en-v1.5"):
+    def __init__(
+        self,
+        model_name="BAAI/bge-base-en-v1.5",
+        index_dir="indexes"
+    ):
 
         self.model = SentenceTransformer(model_name)
 
         self.documents = None
         self.index = None
         self.embeddings = None
+
+        self.index_dir = index_dir
+
+        os.makedirs(
+            self.index_dir,
+            exist_ok=True
+        )
+
+    # ====================================================
+    # Build Index
+    # ====================================================
 
     def build(self, documents):
 
@@ -33,12 +49,68 @@ class DenseRetriever:
 
         self.index.add(self.embeddings)
 
+    # ====================================================
+    # Save
+    # ====================================================
+
+    def save(self):
+
+        faiss.write_index(
+            self.index,
+            os.path.join(
+                self.index_dir,
+                "dense.index"
+            )
+        )
+
+        np.save(
+            os.path.join(
+                self.index_dir,
+                "dense_embeddings.npy"
+            ),
+            self.embeddings
+        )
+
+    # ====================================================
+    # Load
+    # ====================================================
+
+    def load(self, documents):
+
+        self.documents = documents
+
+        self.index = faiss.read_index(
+
+            os.path.join(
+                self.index_dir,
+                "dense.index"
+            )
+
+        )
+
+        self.embeddings = np.load(
+
+            os.path.join(
+                self.index_dir,
+                "dense_embeddings.npy"
+            )
+
+        )
+
+    # ====================================================
+    # Search
+    # ====================================================
+
     def search(self, query, top_k=20):
 
         query_embedding = self.model.encode(
+
             [query],
+
             normalize_embeddings=True,
+
             convert_to_numpy=True
+
         ).astype(np.float32)
 
         scores, indices = self.index.search(
@@ -46,7 +118,9 @@ class DenseRetriever:
             top_k
         )
 
-        results = self.documents.iloc[indices[0]].copy()
+        results = self.documents.iloc[
+            indices[0]
+        ].copy()
 
         results["dense_score"] = scores[0]
 
